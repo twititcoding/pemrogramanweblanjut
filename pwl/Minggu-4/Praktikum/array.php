310836d437394beef8e6e3568171baaa4703edee
twititcoding <76032103+twititcoding@users.noreply.github.com>
2021-04-24 21:28:24 +0700

    Add files via upload

---
+++ pwl/Minggu-4/Praktikum/array.php
@@ -0,0 +1,15 @@
+<?php
+
+$arrybuah = array ("mangga","Apel","Pisang","jeruk");
+ 
+echo $arrybuah[0]; //Mangga
+echo $arrybuah[3]; //Jeruk
+$arrWarna = array();
+$arrWarna[] = "Merah";
+$arrWarna[] = "Biru";
+$arrWarna[] = "Hijau";
+$arrWarna[] = "Putih";
+echo $arrWarna[0]; //Merah
+echo $arrWarna[2]; //Hijau
+
+?>
